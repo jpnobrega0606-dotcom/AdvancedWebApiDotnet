2f252529b07f8cdae0664ecbb233c37f9e31728a
bruno <bruno@BRUNO>
2026-08-01 18:09:37 -0300

    Alteradoro sumaries de Freezin para Freezing5

--- AdvancedWebApiDotnet/Controllers/WeatherForecastController.cs
+++ AdvancedWebApiDotnet/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace AdvancedWebApiDotnet.Controllers
     {
         private static readonly string[] Summaries =
         [
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing5", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         ];
 
         [HttpGet(Name = "GetWeatherForecast")]
