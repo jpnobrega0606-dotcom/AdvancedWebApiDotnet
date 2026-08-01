5f55e900e9188efbf793c3461839071cab4a870e
João Paulo da Nóbrega Alves <minsait.jpdnalves@voegol.com.br>
2026-08-01 18:09:35 -0300

    Alterado sumaries de bracing para bracing5

--- AdvancedWebApiDotnet/Controllers/WeatherForecastController.cs
+++ AdvancedWebApiDotnet/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace AdvancedWebApiDotnet.Controllers
     {
         private static readonly string[] Summaries =
         [
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing5", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         ];
 
         [HttpGet(Name = "GetWeatherForecast")]
